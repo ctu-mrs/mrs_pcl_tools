1ce97cb35563c21f04a63116c23fdc7922f62717
Pavel Petracek <petrapa6@fel.cvut.cz>
2020-08-01 18:01:10 +0200

    wip: move TicToc class to support.h

--- include/mrs_pcl_tools/common_includes_and_typedefs.h
+++ include/mrs_pcl_tools/common_includes_and_typedefs.h
@@ -46,30 +46,3 @@ typedef pcl::PointCloud<pt_NORM>  PC_NORM;
 typedef pcl::PointCloud<pt_OS1>   PC_OS1;
 
 //}
-
-/*//{ class TicToc */
-
-class TicToc {
-public:
-  TicToc() {
-    tic();
-  }
-
-  void tic() {
-    start = std::chrono::system_clock::now();
-  }
-
-  double toc() {
-    std::chrono::duration<double> elapsed_seconds = std::chrono::system_clock::now() - start;
-    return elapsed_seconds.count() * 1000;
-  }
-
-  void toc_print(const std::string text) {
-    ROS_INFO("%s: %0.2f ms", text.c_str(), toc());
-  }
-
-private:
-  std::chrono::time_point<std::chrono::system_clock> start;
-};
-
-/*//}*/
--- include/mrs_pcl_tools/support.h
+++ include/mrs_pcl_tools/support.h
@@ -13,3 +13,30 @@ private:
 
 };
 }  // namespace mrs_pcl_tools
+
+/*//{ class TicToc */
+
+class TicToc {
+public:
+  TicToc() {
+    tic();
+  }
+
+  void tic() {
+    start = std::chrono::system_clock::now();
+  }
+
+  double toc() {
+    std::chrono::duration<double> elapsed_seconds = std::chrono::system_clock::now() - start;
+    return elapsed_seconds.count() * 1000;
+  }
+
+  void toc_print(const std::string text) {
+    ROS_INFO("%s: %0.2f ms", text.c_str(), toc());
+  }
+
+private:
+  std::chrono::time_point<std::chrono::system_clock> start;
+};
+
+/*//}*/
