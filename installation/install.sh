cce7a8df7ab720df86200aba5ca489bc9557440e
delta <mrs@mrs.mrs>
2020-08-06 15:39:21 +0200

    add installation script

---
+++ installation/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# fix issue: https://github.com/ethz-asl/lidar_align/issues/16 
+sudo mv /usr/include/flann/ext/lz4.h /usr/include/flann/ext/lz4.h.bak
+sudo mv /usr/include/flann/ext/lz4hc.h /usr/include/flann/ext/lz4.hc.bak
+sudo ln -s /usr/include/lz4.h /usr/include/flann/ext/lz4.h
+sudo ln -s /usr/include/lz4hc.h /usr/include/flann/ext/lz4hc.h
