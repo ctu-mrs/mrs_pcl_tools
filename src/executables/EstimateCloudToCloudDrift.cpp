2f712e16188d3eeda122c95d00b7c10521ac7f7a
Pavel Petracek <petrapa6@fel.cvut.cz>
2020-11-29 18:57:34 +0100

    improve parameters loading in estimate_cloud_to_cloud_drift, add sampling of trajectory samples by time

--- src/executables/EstimateCloudToCloudDrift.cpp
+++ src/executables/EstimateCloudToCloudDrift.cpp
@@ -11,24 +11,56 @@
 #include <fstream>
 #include <iomanip>
 
+/*//{ printHelp() */
 void printHelp() {
   ROS_ERROR(
-      "Syntax is: `rosrun mrs_pcl_tools estimate_cloud_to_cloud_drift source.pcd target.pcd trajectory_in.txt trajectory_out.txt [traj_step, crop_dist]`");
+      "Syntax is: `rosrun mrs_pcl_tools estimate_cloud_to_cloud_drift source.pcd target.pcd trajectory_in.txt trajectory_out.txt [options (see below)]`");
   ROS_ERROR(
       "Ground truth trajectory will be estimated from given drifting trajectory using two PCD maps. 1) source cloud (i.e., SLAM map output) and 2) target "
       "cloud (i.e., reference 3D scan). The registration parameters are hard-coded.");
   ROS_ERROR("IMPORTANT: Method expects source/target clouds to be pre-registered and the input trajectory to be in the frame of the source point cloud.");
   ROS_ERROR("Arguments:");
-  ROS_ERROR("- source.pcd:         source point cloud (with drift)");
-  ROS_ERROR("- target.pcd:         target point cloud (ground truth)");
-  ROS_ERROR("- trajectory_in.txt:  trajectory of the robot (expected format: time x y z [qx qy qz qw]");
-  ROS_ERROR("- trajectory_out.txt: corrected trajectory of the robot");
-  ROS_ERROR("- traj_step:          sampling of trajectory by distance, used if greater than 0.0 (optional, default: -1.0 m)");
+  ROS_ERROR(" source.pcd:         source point cloud (with drift)");
+  ROS_ERROR(" target.pcd:         target point cloud (ground truth)");
+  ROS_ERROR(" trajectory_in.txt:  trajectory of the robot (expected format: time x y z [qx qy qz qw]");
+  ROS_ERROR(" trajectory_out.txt: corrected trajectory of the robot");
+  ROS_ERROR(" --traj-step-dist:   sampling of trajectory by distance, used if greater than 0.0 (optional, default: 0.0 m)");
+  ROS_ERROR(" --traj-step-time:   sampling of trajectory by time, used if greater than 0.0 (optional, default: 0.0 s)");
   ROS_ERROR(
-      "- crop_dist:          crop distance of global->local clouds conversion before their local registration (optional, default: [xy: 10.0 m, z: 3 * xy])");
-  ROS_ERROR("- start_time:         start time offset of the trajectory (optional, default: 0.0 s)");
+      " --crop-dist:        crop distance of global->local clouds conversion before their local registration (optional, default: [xy: 10.0 m, z: 3 * xy])");
+  ROS_ERROR(" --start-time:       start time offset of the trajectory (optional, default: 0.0 s)");
 }
+/*//}*/
 
+/*//{ struct ARGUMENTS */
+struct ARGUMENTS
+{
+  bool        initialized = false;
+  std::string pcd_source;
+  std::string pcd_target;
+  std::string txt_trajectory_in;
+  std::string txt_trajectory_out;
+  float       traj_step_dist = 0.0f;
+  float       traj_step_time = 0.0f;
+  float       crop_dist_xy   = 10.0f;
+  float       crop_dist_z    = 30.0f;
+  double      start_time     = 0.0;
+
+  void print() {
+    if (initialized) {
+      ROS_INFO("Source pcd: %s", pcd_source.c_str());
+      ROS_INFO("Target pcd: %s", pcd_target.c_str());
+      ROS_INFO("Trajectory in: %s", txt_trajectory_in.c_str());
+      ROS_INFO("Trajectory out: %s", txt_trajectory_out.c_str());
+      ROS_INFO("Trajectory distance step: %0.2f; time step: %0.2f", traj_step_dist, traj_step_time);
+      ROS_INFO("Clouds crop distance -> xy: %0.2f, z: %0.2f", crop_dist_xy, crop_dist_z);
+      ROS_INFO("Start time offset: %0.2f", start_time);
+    }
+  }
+};
+/*//}*/
+
+/*//{ struct TRAJECTORY_POINT */
 struct TRAJECTORY_POINT
 {
   Eigen::Matrix4f transformation;
@@ -39,7 +71,43 @@ struct TRAJECTORY_POINT
   float           eucl_distance_from_origin;
   float           distance_on_trajectory;
 };
+/*//}*/
 
+/*//{ parseArguments() */
+bool parseArguments(int argc, char **argv, ARGUMENTS &args) {
+  if (argc < 5) {
+    printHelp();
+    return false;
+  }
+
+  args.pcd_source         = argv[1];
+  args.pcd_target         = argv[2];
+  args.txt_trajectory_in  = argv[3];
+  args.txt_trajectory_out = argv[4];
+
+  for (int i = 5; i < argc; i += 2) {
+    const auto option = std::string(argv[i]);
+    if (option == "--traj-step-dist") {
+      args.traj_step_dist = std::atof(argv[i + 1]);
+    } else if (option == "--traj-step-time") {
+      args.traj_step_time = std::atof(argv[i + 1]);
+    } else if (option == "--crop-dist") {
+      args.crop_dist_xy = std::atof(argv[i + 1]);
+      args.crop_dist_z  = 3.0f * args.crop_dist_xy;
+    } else if (option == "--start-time") {
+      args.start_time = std::atof(argv[i + 1]);
+    } else {
+      std::cerr << "Unknown option: " << option.c_str() << ". Skipping." << std::endl;
+    }
+  }
+
+  args.initialized = true;
+
+  return true;
+}
+/*//}*/
+
+/* -------------------- Global variables -------------------- */
 pcl::IterativeClosestPoint<pt_XYZ, pt_XYZ> _icp;
 pcl::CropBox<pt_XYZ>                       _filter_box;
 
@@ -51,6 +119,7 @@ ros::Publisher _pub_pc_aligned_local;
 ros::Publisher _pub_path_untransformed;
 ros::Publisher _pub_path_transformed;
 
+/* -------------------- Functions -------------------- */
 /*//{ eigenMatrixToPoseMsg() */
 geometry_msgs::Pose eigenMatrixToPoseMsg(const Eigen::Matrix4f &mat) {
   geometry_msgs::Pose msg;
@@ -198,8 +267,7 @@ std::optional<std::tuple<Eigen::Matrix4f, PC::Ptr>> registerClouds(const PC::Ptr
 
 /*//{ estimateGroundTruthTrajectory() */
 std::vector<TRAJECTORY_POINT> estimateGroundTruthTrajectory(const std::vector<std::tuple<double, Eigen::Matrix4f>> &trajectory, const PC::Ptr &pc_source,
-                                                            const PC::Ptr &pc_target, const float &traj_step, const float &crop_dist,
-                                                            const double &start_time) {
+                                                            const PC::Ptr &pc_target, const ARGUMENTS &args) {
 
   std::vector<TRAJECTORY_POINT> drift_data;
 
@@ -219,12 +287,10 @@ std::vector<TRAJECTORY_POINT> estimateGroundTruthTrajectory(const std::vector<st
   _icp.setRANSACOutlierRejectionThreshold(0.8);
   _icp.setUseReciprocalCorrespondences(false);
 
-  const float xy_crop_dist = crop_dist / 2.0f;
-  const float z_crop_dist  = 3.0f * crop_dist / 2.0f;
-
   const auto [t_origin, pose_origin]      = trajectory.front();
   const Eigen::Vector3f position_origin   = Eigen::Vector3f(pose_origin(0, 3), pose_origin(1, 3), pose_origin(2, 3));
-  const double          time_min          = t_origin + start_time;
+  const double          time_min          = t_origin + args.start_time;
+  double                time_prev         = time_min;
   Eigen::Vector3f       position_prev     = position_origin;
   float                 trajectory_length = 0.0f;
   unsigned int          sample_count      = 0;
@@ -241,19 +307,20 @@ std::vector<TRAJECTORY_POINT> estimateGroundTruthTrajectory(const std::vector<st
     const auto [t, pose]           = trajectory.at(i);
     const Eigen::Vector3f position = Eigen::Vector3f(pose(0, 3), pose(1, 3), pose(2, 3));
 
-    const float d_position = (position - position_prev).norm();
+    const float  d_position = (position - position_prev).norm();
+    const double d_time     = t - time_prev;
     trajectory_length += d_position;
 
-    // Discretize trajectory by distance per traj_step
-    if ((i > 0 && d_position < traj_step) || t < time_min) {
+    // Discretize trajectory by distance/time per traj_step
+    if ((i > 0 && d_position < args.traj_step_dist && d_time < args.traj_step_time) || t < time_min) {
       continue;
     }
 
     ROS_INFO("- %d/%ld.", i, trajectory.size());
 
     // Crop both clouds
-    const PC::Ptr pc_source_local = cropCloud(pc_source, position, xy_crop_dist, z_crop_dist);
-    const PC::Ptr pc_target_local = cropCloud(pc_target, position, xy_crop_dist, z_crop_dist);
+    const PC::Ptr pc_source_local = cropCloud(pc_source, position, args.crop_dist_xy, args.crop_dist_z);
+    const PC::Ptr pc_target_local = cropCloud(pc_target, position, args.crop_dist_xy, args.crop_dist_z);
 
     if (pc_source_local->empty() || pc_target_local->empty()) {
       ROS_WARN("Cropped part of the source or target clouds are empty, skipping frame.");
@@ -316,6 +383,7 @@ std::vector<TRAJECTORY_POINT> estimateGroundTruthTrajectory(const std::vector<st
       T_prev = T;
     }
 
+    time_prev     = t;
     position_prev = position;
   }
 
@@ -324,50 +392,27 @@ std::vector<TRAJECTORY_POINT> estimateGroundTruthTrajectory(const std::vector<st
 /*//}*/
 
 int main(int argc, char **argv) {
-  if (argc < 5) {
-    printHelp();
+
+  ARGUMENTS args;
+  if (!parseArguments(argc, argv, args)) {
     return -1;
   }
 
   ros::init(argc, argv, "EstimateCloudToCloudDrift");
   ros::NodeHandle nh("EstimateCloudToCloudDrift");
 
-  // open rosbag
-  const std::string pcd_source         = argv[1];
-  const std::string pcd_target         = argv[2];
-  const std::string txt_trajectory_in  = argv[3];
-  const std::string txt_trajectory_out = argv[4];
-  float             traj_step          = -1.0f;
-  float             crop_dist          = 10.0f;
-  double            start_time         = 0.0;
-  if (argc == 6) {
-    traj_step = std::atof(argv[5]);
-  } else if (argc == 7) {
-    traj_step = std::atof(argv[5]);
-    crop_dist = std::atof(argv[6]);
-  } else if (argc == 8) {
-    traj_step  = std::atof(argv[5]);
-    crop_dist  = std::atof(argv[6]);
-    start_time = std::atof(argv[7]);
-  }
-
-  ROS_INFO("Source pcd: %s", pcd_source.c_str());
-  ROS_INFO("Target pcd: %s", pcd_target.c_str());
-  ROS_INFO("Trajectory in: %s", txt_trajectory_in.c_str());
-  ROS_INFO("Trajectory out: %s", txt_trajectory_out.c_str());
-  ROS_INFO("Trajectory distance step: %0.2f", traj_step);
-  ROS_INFO("Clouds crop distance -> xy: %0.2f, z: %0.2f", crop_dist, crop_dist * 3.0f);
+  args.print();
 
   // load trajectory
-  std::vector<std::tuple<double, Eigen::Matrix4f>> trajectory = loadTrajectory(txt_trajectory_in);
+  std::vector<std::tuple<double, Eigen::Matrix4f>> trajectory = loadTrajectory(args.txt_trajectory_in);
   if (trajectory.size() == 0) {
     ROS_ERROR("Loaded trajectory is empty. Ending the node.");
     return 0;
   }
 
   // load pcds
-  const auto ret_source = mrs_pcl_tools::loadPcXYZ(pcd_source);
-  const auto ret_target = mrs_pcl_tools::loadPcXYZ(pcd_target);
+  const auto ret_source = mrs_pcl_tools::loadPcXYZ(args.pcd_source);
+  const auto ret_target = mrs_pcl_tools::loadPcXYZ(args.pcd_target);
 
   if (!ret_source || !ret_target) {
     ROS_ERROR("Could not load one of the source/target sources.");
@@ -384,10 +429,10 @@ int main(int argc, char **argv) {
   _pub_path_transformed   = nh.advertise<nav_msgs::Path>("trajectory/transformed", 1);
 
   // for traj_step in trajectory, crop maps and compute icp source->target
-  std::vector<TRAJECTORY_POINT> data = estimateGroundTruthTrajectory(trajectory, ret_source.value(), ret_target.value(), traj_step, crop_dist, start_time);
+  std::vector<TRAJECTORY_POINT> data = estimateGroundTruthTrajectory(trajectory, ret_source.value(), ret_target.value(), args);
 
   // save trajectory
-  saveTrajectory(txt_trajectory_out, data);
+  saveTrajectory(args.txt_trajectory_out, data);
 
   return 0;
 }
