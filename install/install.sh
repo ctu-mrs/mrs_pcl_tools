ecefda650feba2eacbd18b66dee33757fd9748b5
Tomas Baca <klaxalk@gmail.com>
2021-10-13 16:14:11 +0200

    generalized install script

--- install/install.sh
+++ install/install.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+set -e
+
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
+
+# get the path to this script
+MY_PATH=`dirname "$0"`
+MY_PATH=`( cd "$MY_PATH" && pwd )`
+cd "$MY_PATH"
+
 unattended=0
 for param in "$@"
 do
