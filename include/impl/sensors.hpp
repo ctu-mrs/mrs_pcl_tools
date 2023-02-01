e6d8d69f49e007d87d4ad1d3c797a8c28ee5c04e
Yurii Stasinchuk <stasiyur@fel.cvut.cz>
2023-02-01 17:56:31 +0100

    Filtering data which is too close to the drone out from rgbd data

--- include/impl/sensors.hpp
+++ include/impl/sensors.hpp
@@ -1,5 +1,6 @@
 
 /* class SensorDepthCamera */
+#include <mrs_pcl_tools/PCLFiltration.h>
 
 /*//{ initialize() */
 void SensorDepthCamera::initialize(const ros::NodeHandle& nh, const std::shared_ptr<CommonHandlers_t> common_handlers, const std::string& prefix,
@@ -29,6 +30,9 @@ void SensorDepthCamera::initialize(const ros::NodeHandle& nh, const std::shared_
 
   _common_handlers->param_loader->loadParam("depth/" + sensor_name + "/filter/voxel_grid/resolution", voxel_grid_resolution, 0.0f);
   voxel_grid_use = voxel_grid_resolution > 0.0f;
+
+  _common_handlers->param_loader->loadParam("depth/" + sensor_name + "/filter/close", filter_close_use, true);
+  _common_handlers->param_loader->loadParam("depth/" + sensor_name + "/filter/close_min", filter_close_min, 0.2f);
 
   _common_handlers->param_loader->loadParam("depth/" + sensor_name + "/filter/radius_outlier/radius", radius_outlier_radius, 0.0f);
   _common_handlers->param_loader->loadParam("depth/" + sensor_name + "/filter/radius_outlier/neighbors", radius_outlier_neighbors, 0);
@@ -203,12 +207,12 @@ void SensorDepthCamera::convertDepthToCloudOrdered(const sensor_msgs::Image::Con
       // Convert to point cloud points and optionally clip range
       if (valid && range_clip_use && (invalid_close || invalid_far)) {
         depth = 0.0f;
-      } 
-
-      if (!valid) 
-      {
-        depth = -1.0;
       }
+
+      /* if (!valid) */
+      /* { */
+      /*   depth = -1.0; */
+      /* } */
       imagePointToCloudPoint(u, v, depth, out_pc->at(col, row));
 
       if (range_clip_use && ((return_removed_close && invalid_close) || (return_removed_far && invalid_far))) {
@@ -275,6 +279,31 @@ void SensorDepthCamera::process_depth_msg(mrs_lib::SubscribeHandler<sensor_msgs:
   diag_msg->vfov                                    = vfov;
 
   // Apply filters to the original cloud (beware, the filters are applied in sequential order: no parallelization)
+  if (filter_close_use) {
+    // filtering out close
+    using pt_t                              = typename PC::PointType;
+    const auto [range_exists, range_offset] = mrs_pcl_tools::getFieldOffset<pt_t>("range");
+
+    for (auto& point : cloud->points) {
+      if (range_exists) {
+        const auto range = getFieldValue<uint32_t>(point, range_offset);
+        if (range < filter_close_min) {
+          point.x = std::numeric_limits<float>::quiet_NaN();
+          point.y = std::numeric_limits<float>::quiet_NaN();
+          point.z = std::numeric_limits<float>::quiet_NaN();
+        }
+      } else {
+        const vec3_t pt       = point.getArray3fMap();
+        const float  range_sq = pt.squaredNorm();
+        if (range_sq < filter_close_min) {
+          point.x = std::numeric_limits<float>::quiet_NaN();
+          point.y = std::numeric_limits<float>::quiet_NaN();
+          point.z = std::numeric_limits<float>::quiet_NaN();
+        }
+      }
+    }
+  }
+
   if (voxel_grid_use) {
     cloud = mrs_pcl_tools::filters::applyVoxelGridFilter(cloud, voxel_grid_resolution);
   }
