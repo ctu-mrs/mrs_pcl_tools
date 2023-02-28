94de3eef830ad4370d0dd579b3ab94a52cd4e08f
Pavel Petracek <petrapa6@fel.cvut.cz>
2023-02-28 16:16:22 +0100

    Move point-offset methods to support.h

--- src/PCLFiltration.cpp
+++ src/PCLFiltration.cpp
@@ -2,31 +2,6 @@
 
 namespace mrs_pcl_tools
 {
-
-/*//{ getFieldOffset() */
-template <typename pt_t>
-std::tuple<bool, std::size_t> getFieldOffset(const std::string& field_name) {
-  std::vector<pcl::PCLPointField> fields;
-  const int                       field_idx = pcl::getFieldIndex<pt_t>(field_name, fields);
-  if (field_idx == -1)
-    return {false, 0};
-  const std::size_t field_offset = fields.at(field_idx).offset;
-  return {true, field_offset};
-}
-/*//}*/
-
-/*//{ getFieldValue() */
-template <typename T, typename pt_t>
-T getFieldValue(const pt_t& point, std::size_t field_offset) {
-  /* T val; */
-  /* pcl::getFieldValue(point, field_offset, val); */
-  const std::uint8_t* pt_data     = reinterpret_cast<const std::uint8_t*>(&(point));
-  T                   field_value = 0;
-  memcpy(&field_value, pt_data + field_offset, sizeof(T));
-  return field_value;
-}
-/*//}*/
-
 /* onInit() //{ */
 void PCLFiltration::onInit() {
 
