cd8c8e70c7e77c7b95283b97224cf880d7c7d99d
Pavel Petráček <petracekpav@gmail.com>
2025-02-01 11:47:01 +0100

    Revert "Implement node to convert Livox CustomMsg to ROS PointCloud2. (#3)"
    This reverts commit c14f209a663173a068a8aeabcbb3cfc512e1bedb.

--- src/support.cpp
+++ src/support.cpp
@@ -48,65 +48,6 @@ void savePCD(const std::string &pcd_file, const sensor_msgs::PointCloud2::Ptr &c
 }
 /*//}*/
 
-/*//{ ConvertLivoxPCLtoPCL2() */
-void convertLivoxPCLtoPCL2(const sensor_msgs::PointCloud2::Ptr &cloud, const livox_ros_driver2::CustomMsg::ConstPtr &livox_msg) {
-  cloud->header = livox_msg->header;
-  cloud->fields.resize(6);
-
-  cloud->fields[0].offset   = 0;
-  cloud->fields[0].name     = "x";
-  cloud->fields[0].count    = 1;
-  cloud->fields[0].datatype = sensor_msgs::PointField::FLOAT32;
-
-  cloud->fields[1].offset   = 4;
-  cloud->fields[1].name     = "y";
-  cloud->fields[1].count    = 1;
-  cloud->fields[1].datatype = sensor_msgs::PointField::FLOAT32;
-
-  cloud->fields[2].offset   = 8;
-  cloud->fields[2].name     = "z";
-  cloud->fields[2].count    = 1;
-  cloud->fields[2].datatype = sensor_msgs::PointField::FLOAT32;
-
-  cloud->fields[3].offset   = 12;
-  cloud->fields[3].name     = "intensity";
-  cloud->fields[3].count    = 1;
-  cloud->fields[3].datatype = sensor_msgs::PointField::FLOAT32;
-
-  cloud->fields[4].offset   = 16;
-  cloud->fields[4].name     = "tag";
-  cloud->fields[4].count    = 1;
-  cloud->fields[4].datatype = sensor_msgs::PointField::UINT8;
-
-  cloud->fields[5].offset   = 17;
-  cloud->fields[5].name     = "line";
-  cloud->fields[5].count    = 1;
-  cloud->fields[5].datatype = sensor_msgs::PointField::UINT8;
-
-  cloud->point_step         = 18;
-  cloud->row_step           = cloud->point_step * livox_msg->point_num;
-  cloud->data.resize(cloud->row_step);
-
-  uint8_t* livox_data_ptr = cloud->data.data();
-    for (const auto& point : livox_msg->points)
-    {
-        *(reinterpret_cast<float*>(livox_data_ptr + 0)) = point.x;
-        *(reinterpret_cast<float*>(livox_data_ptr + 4)) = point.y;
-        *(reinterpret_cast<float*>(livox_data_ptr + 8)) = point.z;
-        *(reinterpret_cast<float*>(livox_data_ptr + 12)) = static_cast<float>(point.reflectivity);
-        *(livox_data_ptr + 16) = point.tag;
-        *(livox_data_ptr + 17) = point.line;
-
-        livox_data_ptr += cloud->point_step;
-    }
-
-    cloud->width = livox_msg->point_num;
-    cloud->height = 1;
-    cloud->is_bigendian = false;
-    cloud->is_dense = true;
-}
-/*//}*/
-
 /*//{ estimateNormals() */
 PC_NORM::Ptr estimateNormals(const PC::Ptr &cloud, const float &normal_est_radius) {
 
