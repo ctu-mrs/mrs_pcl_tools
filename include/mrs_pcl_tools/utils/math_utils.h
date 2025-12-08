9349e1370d10e6600302ff4a08b6e108d7a238a7
TomasMerva <tmerva7@gmail.com>
2025-12-08 16:17:28 +0100

    ft: basic math utils

--- include/mrs_pcl_tools/utils/math_utils.h
+++ include/mrs_pcl_tools/utils/math_utils.h
@@ -1,12 +1,12 @@
 #pragma once
 
 #include <mrs_pcl_tools/utils/common_includes_and_typedefs.h>
-
+#include <mrs_lib/attitude_converter.h>
 
 namespace mrs_pcl_tools
 {
 
-void printEigenMatrix(const Eigen::Matrix4f& mat, const std::string& prefix = "");
+void printEigenMatrix(ILogger& logger, const Eigen::Matrix4f& mat, const std::string& prefix = "");
 
 Eigen::Matrix4f getRotationMatrixAroundPoint(const Eigen::Matrix3f& rotation, const Eigen::Vector4f& point);
 
