410133d084ef5301e2b3771bc44c55ca9e2d37ec
TomasMerva <tmerva7@gmail.com>
2025-12-09 09:04:19 +0100

    ft: visualization functions

--- include/mrs_pcl_tools/utils/visualization.h
+++ include/mrs_pcl_tools/utils/visualization.h
@@ -1,1 +1,17 @@
 #pragma once
+
+#include <mrs_pcl_tools/utils/common_includes_and_typedefs.h>
+#include <std_msgs/msg/color_rgba.hpp>
+
+namespace mrs_pcl_tools
+{
+
+namespace visualization
+{
+
+PC_RGB::Ptr colorizeCloud(const PC::Ptr& cloud_xyz);
+std_msgs::msg::ColorRGBA heightToRGBA(double& height, const double& alpha = 1.0);
+
+}  // namespace visualization
+
+}  // namespace mrs_pcl_tools
